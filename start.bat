49679b1ab0576291f454a09af8b330417562436d
JatinThakur-797 <jt6285638@gmail.com>
2026-06-11 10:34:10 +0530

    feat: add .env-example and update start.bat to load local .env variables

--- start.bat
+++ start.bat
@@ -1,7 +1,16 @@
 @echo off
 
+if exist .env (
+    echo Loading environment variables from .env...
+    for /f "usebackq eol=# tokens=1* delims==" %%i in (".env") do (
+        set %%i=%%j
+    )
+) else (
+    echo WARNING: .env file not found! Using system defaults.
+)
+
 echo Starting Backend...
-start cmd /k "cd SmartSelectBackend && mvnw spring-boot:run"
+start cmd /k "cd SmartSelectBackend && .\mvnw spring-boot:run"
 
 timeout /t 5
 
